54f67b9bfe78057a7d4e95e893933dbcdc6f2661
Lokesh Jain <jainlokesh318@gmail.com>
2024-08-03 12:59:23 +0530

    init tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     
+    <h1 className="text-3xl text-red-500">
+      Hello world!
+    </h1>
     
     </>
   )
