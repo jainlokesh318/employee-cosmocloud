366ddfefcf53f2f4185f9dd2262f5c7ca76cb520
Lokesh Jain <jainlokesh318@gmail.com>
2024-08-04 19:01:37 +0530

    feat:pagination ui implemented

--- src/components/EmployeeList.tsx
+++ src/components/EmployeeList.tsx
@@ -1,12 +1,28 @@
 import { useEffect } from "react";
 import Loader from "./ui/Loader";
-import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { ArrowLeftIcon, ArrowRightIcon, PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/16/solid";
 import { useEmployee } from "../hooks/useEmployee";
 import { useNavigate } from "react-router-dom";
 
 export default function EmployeeList() {
-    const { fetchEmployeesData, employees, loading, deleteEmployeeData } = useEmployee()
+    const { fetchEmployeesData, employees, loading, deleteEmployeeData, pageDetails: { nextOffset, total, previousOffset } } = useEmployee()
     const navigate = useNavigate();
+
+    const hasPreviousPage = previousOffset >= 0
+    const hasNextPage = nextOffset < total
+
+    const handlePrevPage = () => {
+        if (hasPreviousPage) {
+            fetchEmployeesData(previousOffset)
+        }
+    }
+
+    const handleNextPage = () => {
+        if (hasNextPage) {
+            fetchEmployeesData(nextOffset)
+        }
+    }
+
 
     useEffect(() => {
         fetchEmployeesData();
@@ -14,10 +30,6 @@ export default function EmployeeList() {
 
     if (loading) {
         return <Loader />
-    }
-
-    if (employees.length == 0) {
-        return <div>No Employees found</div>
     }
 
     return (
@@ -43,30 +55,44 @@ export default function EmployeeList() {
                 </thead>
                 <tbody>
                     {
-                        employees.map(({ _id, name }) => {
-                            return (
-                                <tr key={_id} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:text-white cursor-pointer" onClick={() => navigate(`/employee/${_id}`)}>
-                                    <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
-                                        {name}
-                                    </th>
-                                    <td className="px-6 py-4">
-                                        {_id}
-                                    </td>
-                                    <td className="flex gap-2 px-6 py-4">
-                                        <PencilIcon className="size-5 hover:text-gray-200" onClick={(e) => {
-                                            e.stopPropagation();
-                                            navigate(`/employee/edit/${_id}`)
-                                        }} />
-                                        <TrashIcon className="size-5 text-red-300 hover:text-red-500" onClick={(e) => {
-                                            e.stopPropagation();
-                                            deleteEmployeeData(_id)
-                                        }} />
-                                    </td>
-                                </tr>)
-                        }
-                        )
+                        employees.length == 0 ?
+                            <div className="p-4">No Employees found</div> : employees.map(({ _id, name }) => {
+                                return (
+                                    <tr key={_id} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:text-white cursor-pointer" onClick={() => navigate(`/employee/${_id}`)}>
+                                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
+                                            {name}
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            {_id}
+                                        </td>
+                                        <td className="flex gap-2 px-6 py-4">
+                                            <PencilIcon className="size-5 hover:text-gray-200" onClick={(e) => {
+                                                e.stopPropagation();
+                                                navigate(`/employee/edit/${_id}`)
+                                            }} />
+                                            <TrashIcon className="size-5 text-red-300 hover:text-red-500" onClick={(e) => {
+                                                e.stopPropagation();
+                                                deleteEmployeeData(_id)
+                                            }} />
+                                        </td>
+                                    </tr>)
+                            }
+                            )
                     }
+
                 </tbody>
+                <tfoot className="bg-gray-50 dark:bg-gray-800 w-full">
+                    <tr>
+                        <td className="px-4 py-2 w-full">
+                            {`Showing ${nextOffset < total ? nextOffset : total} of ${total} entries `}
+                        </td>
+                        <td></td>
+                        <td className="px-4 py-2 flex gap-2 text-right justify-end">
+                            <ArrowLeftIcon className={`size-5 ${hasPreviousPage ? 'text-white cursor-pointer' : 'text-gray-600 cursor-not-allowed'}`} onClick={handlePrevPage} />
+                            <ArrowRightIcon className={`size-5 ${hasNextPage ? 'text-white cursor-pointer' : 'text-gray-600 cursor-not-allowed'}`} onClick={handleNextPage} />
+                        </td>
+                    </tr>
+                </tfoot>
             </table>
         </div>
 
